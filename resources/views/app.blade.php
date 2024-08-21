2b0955105f68b5bf6c83af362d9ce03af20e8dff
ballzxs <ballnysiharath@gmail.com>
2024-08-21 17:32:06 +0700

    Create Project Laravel + Vue VDO 01

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minipos 18</title>
+
+    @vite('resources/css/app.css')
+
+ </head>
+ <body>
+
+    <h1>Laravel App dawdawd</h1>
+    <hr>
+
+    <div id='app'></div>
+    
+
+    @vite('resources/js/app.js')
+ </body>
+ </html>
